d19fe38637cfd07e27b16b94a927f955109a62c4
Jack <jckkempton@gmail.com>
2020-09-28 18:22:30 -0600

    PseudoCoded function to grab info from user.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var howManyChars = 0;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +9,15 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+function getInfo() {
+// Get Password Length, Prompt 8 to 128 Characters
+var getPasswordLength = prompt("How Long Would You Like Your Password? 8 - 128 Characters.");
+// Get Password Special Characters, Confirm
+// Get Password UpperCase Characters, Confirm
+// Get Password LowerCase Characters, Confirm
+// Get Password Numbers, Confirm
+console.log(getPasswordLength);
+}
+getInfo()
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
