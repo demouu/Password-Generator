083767fe7f5a7a1a2c70b85c5674268ff4eabef5
Jack <jckkempton@gmail.com>
2020-09-28 21:51:56 -0600

    Set Rules for getPasswordLength

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ function writePassword() {
 }
 function getInfo() {
 // Get Password Length, Prompt 8 to 128 Characters
-var getPasswordLength = prompt("How Long Would You Like Your Password? 8 - 128 Characters.");
+var getPasswordLength = parseInt(prompt("How Long Would You Like Your Password? 8 - 128 Characters."));
+//If statement to get a number
+if (isNaN(getPasswordLength)) {
+  alert("Try Again, Enter a Number.");
+  getInfo();
+} else if (getPasswordLength < 8 || getPasswordLength > 128) {
+  alert("Try Again, Enter a Number Between 8 and 128.");
+  getInfo();
+}
 // Get Password Special Characters, Confirm
 // Get Password UpperCase Characters, Confirm
 // Get Password LowerCase Characters, Confirm
